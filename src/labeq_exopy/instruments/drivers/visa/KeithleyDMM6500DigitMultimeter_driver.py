7ad8e1143c4f58744586160a6b0c9f728db72774
FoxGreatNews <71724518+FoxGreatNews@users.noreply.github.com>
2022-07-05 14:49:18 -0400

    enter

--- src/labeq_exopy/instruments/drivers/visa/KeithleyDMM6500DigitMultimeter_driver.py
+++ src/labeq_exopy/instruments/drivers/visa/KeithleyDMM6500DigitMultimeter_driver.py
@@ -23,44 +23,11 @@ class Keithley6500(VisaInstrument):
         self.write_termination = '\n'
         self.read_termination = '\n'
 
-    @instrument_property
     @secure_communication()
-    def function(self):
-        self.read_termination = '\n'
-
-        """Function setter
-
-        """
-        value = self.query('FUNCtion?\n')
-        if value:
-            return value
-        else:
-            raise InstrIOError('Keithley6500 : Failed to return function')
-
-    @function.setter
-    @secure_communication()
-    def function(self, value):
-
-        self.write('FUNCtion "{}"'.format(value))
-        # The Keithley returns "VOLT:DC" needs to remove the quotes
-        if not(self.query('FUNCtion?\n')[1:-1].lower() == value.lower()):
-            raise InstrIOError('Keithley6500: Failed to set function')
-
-    @secure_communication()
-    def read_voltage_dc(self,mes_range = 'DEF', mes_resolution = 'DEF'):
-
-        """Return the DC voltage read by the instrument.
-
-        Perform a direct reading without any waiting. Can return identical
-        values if the instrument is read more often than its integration time.
-        The arguments are unused and here only to make this driver and the
-        agilent driver compatible.
-
-        """
-        
-       
-        self.write('SENS:FUNC "VOLT:DC"')
-        value = self.query('MEAS?')
+    def read_voltage_dc(self):
+    # The SENS:FUNC setting may be circumvented by using MEAS:FUNC ('SENS:FUNC "VOLT:DC"')
+    #MEAS:FUNC is a coupling of the sens:func and data?
+        value = self.query('MEAS:VOLT:DC?')
 
 
         #split string into list to get voltage measurement
@@ -75,24 +42,9 @@ class Keithley6500(VisaInstrument):
             raise InstrIOError('Keithley6500: DC voltage measure failed')
 
     @secure_communication()
-    def read_voltage_ac(self,mes_range = 'DEF', mes_resolution = 'DEF'):    #need to test if extra parameters are needed
-        """Return the AC voltage read by the instrument.
-
-        Perform a direct reading without any waiting. Can return identical
-        values if the instrument is read more often than its integration time.
-        The arguments are unused and here only to make this driver and the
-        agilent driver compatible.
-
-        """
-
-        self.write('SENS:FUNC "VOLT:AC"')
-        value = self.query('MEAS?')
-        
-
-        #split string into list to get voltage measurement
+    def read_voltage_ac(self):    
+        value = self.query('MEAS:VOLT:AC?')
         value = value.split(",")[0]
-
-        #remove "NVAC" string from measurement so we can cast to a float
         value = value.replace("NVAC","")
         
         if value:
@@ -102,26 +54,8 @@ class Keithley6500(VisaInstrument):
 
     @secure_communication()
     def read_two_resistance(self):
-        """
-        Return the two wire resistance read by the instrument.
-
-        Perform a direct reading without any waiting. Can return identical
-        values if the instrument is read more often than its integration time.
-        The arguments are unused and here only to make this driver and the
-        agilent driver compatible.
-
-        """
-        self.write('SENS:FUNC "res"')
-
-
-
-
-        value = self.query('MEAS?')
-
-        #split string into list to get current measurement
+        value = self.query('MEAS:RES?')
         value = value.split(",")[0]
-
-        #remove "NOHM" string from measurement so we can cast to a float
         value = value.replace("NOHM","")
         
         if value:
@@ -131,24 +65,8 @@ class Keithley6500(VisaInstrument):
 
     @secure_communication()
     def read_four_resistance(self):
-        """
-        Return the four wire resistance read by the instrument.
-
-        Perform a direct reading without any waiting. Can return identical
-        values if the instrument is read more often than its integration time.
-        The arguments are unused and here only to make this driver and the
-        agilent driver compatible.
-
-        """
-        self.write('SENS:FUNC "fres"')
-
-
-        value = self.query('MEAS?')
-
-        #split string into list to get current measurement
+        value = self.query('MEAS:FRES?')
         value = value.split(",")[0]
-
-        #remove "NOHM4W" string from measurement so we can cast to a float
         value = value.replace("NOHM4W","")
         
         if value:
@@ -157,26 +75,9 @@ class Keithley6500(VisaInstrument):
             raise InstrIOError('Keithley6500: Four Wire Resistance measure failed')
 
     @secure_communication()
-    def read_current_dc(self,mes_range = 'DEF', mes_resolution = 'DEF'):
-        """Return the DC current read by the instrument.
-
-        Perform a direct reading without any waiting. Can return identical
-        values if the instrument is read more often than its integration time.
-        The arguments are unused and here only to make this driver and the
-        agilent driver compatible.
-
-        """
-        self.write('SENS:FUNC "CURR:DC"')
-
-
-        #query device with meas? command returning comma seperated string with measurement value, timestamp, reading count
-        # and channel
-        value = self.query('meas?')
-
-        #split string into list to get current measurement
+    def read_current_dc(self):
+        value = self.query('meas:CURR:DC?')
         value = value.split(",")[0]
-
-        #remove "NADC" string from measurement so we can cast to a float
         value = value.replace("NADC","")
 
         if value:
@@ -185,23 +86,11 @@ class Keithley6500(VisaInstrument):
             raise InstrIOError('Keithley6500: DC current measure failed')
 
     @secure_communication()
-    def read_current_ac(self,mes_range = 'DEF', mes_resolution = 'DEF'):
-        """Return the AC current read by the instrument.
-
-        Perform a direct reading without any waiting. Can return identical
-        values if the instrument is read more often than its integration time.
-        The arguments are unused and here only to make this driver and the
-        agilent driver compatible.
-
-        """
-        self.write('SENS:FUNC "VOLT:DC"')
-        value = self.query('MEAS?')
-        
-        #split string into list to get current measurement
+    def read_current_ac(self):
+        value = self.query('MEAS:CURR:AC?')
         value = value.split(",")[0]
-
-        #remove "NAAC" string from measurement so we can cast to a float
         value = value.replace("NAAC","")
+
         if value:
             return float(value)
         else:
