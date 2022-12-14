f5426de4cb3d1283047af74d72a2d4864c7b1f79
Frank-Duffy <fduffy0328@gmail.com>
2022-12-14 17:25:39 -0500

    Testing to establish pattern for feature development
    Testing creating a task that creates driver objects and creates data with them using SimpleTask. There is theoretically no limit to the number of devices involved in this type of task, making this approach ideal for multi-instrument task development.

---
+++ src/labeq_exopy/tasks/tasks/multi_instr/Test_tasks.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Authors - Frank Duffy, LaBEQ / Sardashti Research Group, Clemson University
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from time import sleep
+from atom.api import Float, Str, Enum, set_default
+from exopy.tasks.api import InstrumentTask, SimpleTask
+from labeq_exopy.instruments.drivers.visa.Timer_driver import Timer
+
+class TestTask(SimpleTask):
+    """
+        Set current time as start time.
+
+    """
+    # Time to wait before the ramp.
+    wait_time = Float().tag(pref=True)
+
+    database_entries = set_default({'start_time': 0.0})
+
+    wait = set_default({'activated': True})
+
+    def perform(self):
+
+        connection_info = {"resource_name" : "Timer"}
+        timer = Timer(connection_info)
+
+        time = timer.initiate_timer()
+
+        print("HELLO WORLD")
+        self.write_in_database('start_time', time)
+
+        
+
