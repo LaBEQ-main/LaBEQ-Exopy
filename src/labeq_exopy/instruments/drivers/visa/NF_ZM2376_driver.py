55b1033c6116ebf4974de77f164dc006a75817d7
Frank-Duffy <fduffy0328@gmail.com>
2023-02-20 13:03:59 -0500

    added driver file

---
+++ src/labeq_exopy/instruments/drivers/visa/NF_ZM2376_driver.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Authors - Frank Duffy, LaBEQ / Sardashti Research Group, Clemson University
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from tracemalloc import reset_peak
+from ..driver_tools import (InstrIOError, secure_communication, instrument_property)
+from ..visa_tools import VisaInstrument
+
+class ZM2376(VisaInstrument):
+    """Driver for NF ZM2376 LCR Meter.
+
+    """
+
+    def open_connection(self, **para):
+        """Open the connection to the instr using the `connection_str`.
+
+        """
+        super(ZM2376, self).open_connection(**para)
+        self.write_termination = '\n'
+        self.read_termination = '\n'
