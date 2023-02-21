2e666537f233cebfc45c89eb72304a8f8718d28d
Frank-Duffy <fduffy0328@gmail.com>
2023-02-21 12:34:27 -0500

    added init to driver, freq and measure task,  and task views

--- src/labeq_exopy/instruments/drivers/visa/NF_ZM2376_driver.py
+++ src/labeq_exopy/instruments/drivers/visa/NF_ZM2376_driver.py
@@ -15,14 +15,25 @@ class ZM2376(VisaInstrument):
     """Driver for NF ZM2376 LCR Meter.
 
     """
+    
+    def __init__(self, connection_info, caching_allowed=True,
+                caching_permissions={}):
+        super(ZM2376, self).__init__(connection_info, caching_allowed,
+                caching_permissions)
+        self.write_termination = '\n'
+        self.read_termination = '\n'
+
+        # get current pri and sec measurement parameters
+        self.curr_pri = self.query('CALC1:FORM?')
+        self.curr_sec = self.query('CALC2:FORM?')
+
+        print(self.curr_pri)
 
     def open_connection(self, **para):
         """Open the connection to the instr using the `connection_str`.
 
         """
         super(ZM2376, self).open_connection(**para)
-        self.write_termination = '\n'
-        self.read_termination = '\n'
     
     def set_frequency(self, val):
         """ set the source frequency 
@@ -30,19 +41,39 @@ class ZM2376(VisaInstrument):
         """
         self.write('SOUR:FREQ ' + str(val))
 
-    def set_voltage(self, val)
+    def set_voltage(self, val):
         """ set the source voltage level
 
         """
         self.write('SOUR:VOLT:LEV ' + str(val))
 
-    def fetch_measurements(self, val)
+    def fetch_measurements(self, params):
         """ fetch the latest measurements
 
         """
+        # gather requested parameters
+        req_pri = params[0]
+        req_sec = params[1]
+
+        if self.curr_pri != req_pri or self.curr_sec != req_sec:
+            self.set_parameters(req_pri, req_sec)
+
         resp = self.query('FETC?').split(",")
 
         pri = resp[1]
         sec = resp[2]
 
         return pri, sec
+
+    def set_parameters(self, pri, sec):
+        """ set the primary and secondary measurement parameters and update driver attributes """
+
+        self.write('CALC1:FORM '+ pri)
+        self.curr_pri = pri
+
+        self.write('CALC2:FORM '+ sec)
+        self.curr_sec = sec
+
+
+
+
--- src/labeq_exopy/tasks/tasks/instr/NF_ZM2376_tasks.py
+++ src/labeq_exopy/tasks/tasks/instr/NF_ZM2376_tasks.py
@@ -13,19 +13,45 @@ from atom.api import Float, Str, Enum, set_default
 
 from exopy.tasks.api import InstrumentTask, InterfaceableTaskMixin
 
-class NFLockInMeasureTask(InstrumentTask):
-    """ Measure lock in output"""
+class ZM2376_SetFrequencyTask(InstrumentTask):
+
 
     # Time to wait before execution
     wait_time = Float().tag(pref=True)
+    
+    # frequency set point
+    freq = Str().tag(pref=True)
 
-    val = Enum('R', 'Phase', 'X', 'Y').tag(pref=True)
     wait = set_default({'activated': True, 'wait': ['instr']})
     database_entries = set_default({'val': 0.0})
 
     def perform(self):
         
         sleep(self.wait_time)
+        freq_setpoint = self.format_and_eval_string(self.freq)
 
-        num = self.driver.measure(self.val)
-        self.write_in_database('val', num)
+        self.driver.set_frequency(freq_setpoint)
+        self.write_in_database('val', freq_setpoint)
+
+class ZM2376_MeasureTask(InstrumentTask):
+
+    # Time to wait before execution
+    wait_time = Float().tag(pref=True)
+    
+    # pri and sec parameter options
+    pri = Enum('Z','Y','R','RP','RS','G','C','CP','CS','L','LP','LS','REAL','MLIN').tag(pref=True)
+    sec = Enum('Q','D','PHAS','X','B','RS','RP','G','LP','RDC','IMAG','REAL').tag(pref=True)
+
+    wait = set_default({'activated': True, 'wait': ['instr']})
+    database_entries = set_default({'pri': 0.0, 'sec': 0.0})
+
+    def perform(self):
+        
+        sleep(self.wait_time)
+
+        params = [self.pri, self.sec]
+
+        pri_val, sec_val = self.driver.fetch_measurements(params)
+
+        self.write_in_database('pri', pri_val)
+        self.write_in_database('sec', sec_val)
